1bb36740d7d12be5c884f2e19ac9872d46bdf631
SoulCRYSIS <ohm2544@hotmail.com>
2024-01-18 17:17:37 +0700

    add zoom/save

--- lib/import_image.dart
+++ lib/import_image.dart
@@ -4,7 +4,9 @@ import 'dart:ui' as ui;
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:image_downloader_web/image_downloader_web.dart';
 import 'package:stroke_text/stroke_text.dart';
+import 'package:widgets_to_image/widgets_to_image.dart';
 
 class ImportImage extends StatefulWidget {
   const ImportImage({Key? key}) : super(key: key);
@@ -18,8 +20,12 @@ class _ImportImageState extends State<ImportImage> {
   Offset refPoint1 = const Offset(100, 50);
   Offset refPoint2 = const Offset(150, 100);
   double? refLength;
+  bool isSaving = false;
+  double zoom = 1;
 
   final GlobalKey stackKey = GlobalKey();
+  final captureController = WidgetsToImageController();
+  final transformationController = TransformationController();
 
   double distancePixel(Offset p1, Offset p2) {
     return sqrt(pow(p1.dx - p2.dx, 2) + pow(p1.dy - p2.dy, 2));
@@ -43,126 +49,142 @@ class _ImportImageState extends State<ImportImage> {
               child: Center(
             child: image == null
                 ? const Text('unselected')
-                : Stack(
-                    key: stackKey,
-                    children: [
-                      image!,
-                      CustomPaint(
-                        painter: Line(
-                          refPoint1,
-                          refPoint2,
-                        ),
-                      ),
-                      CustomPaint(
-                        painter: Lines(points),
-                      ),
-                      Positioned(
-                        left: refPoint1.dx - 5,
-                        top: refPoint1.dy - 5,
-                        child: Draggable(
-                          feedback: const CirclePoint(),
-                          onDragEnd: (dragDetails) {
-                            RenderBox box = stackKey.currentContext!
-                                .findRenderObject() as RenderBox;
-                            Offset localOffset =
-                                box.globalToLocal(dragDetails.offset);
-                            setState(() {
-                              refPoint1 = localOffset + const Offset(5, 5);
-                            });
-                          },
-                          child: const CirclePoint(),
-                        ),
-                      ),
-                      Positioned(
-                        left: refPoint2.dx - 5,
-                        top: refPoint2.dy - 5,
-                        child: Draggable(
-                          feedback: const CirclePoint(),
-                          onDragEnd: (dragDetails) {
-                            RenderBox box = stackKey.currentContext!
-                                .findRenderObject() as RenderBox;
-                            Offset localOffset =
-                                box.globalToLocal(dragDetails.offset);
-                            setState(() {
-                              refPoint2 = localOffset + const Offset(5, 5);
-                            });
-                          },
-                          child: const CirclePoint(),
-                        ),
-                      ),
-                      Positioned(
-                        left: refPoint1.dx,
-                        top: refPoint1.dy,
-                        child: Transform.rotate(
-                          alignment: Alignment.topLeft,
-                          angle: atan2(refPoint2.dy - refPoint1.dy,
-                              refPoint2.dx - refPoint1.dx),
-                          child: Container(
-                            alignment: Alignment.center,
-                            width: distancePixel(refPoint1, refPoint2),
-                            child: StrokeText(
-                              strokeWidth: 2,
-                              text: distance(refPoint1, refPoint2)
-                                  .toStringAsFixed(1),
+                : InteractiveViewer(
+                    transformationController: transformationController,
+                    onInteractionUpdate: (details) {
+                      setState(() {
+                        zoom =
+                            transformationController.value.getMaxScaleOnAxis();
+                      });
+                    },
+                    child: WidgetsToImage(
+                      controller: captureController,
+                      child: Stack(
+                        key: stackKey,
+                        children: [
+                          image!,
+                          CustomPaint(
+                            painter: Line(
+                              refPoint1,
+                              refPoint2,
                             ),
                           ),
-                        ),
-                      ),
-                      for (List<Offset> point in points) ...[
-                        Positioned(
-                          left: point[0].dx - 5,
-                          top: point[0].dy - 5,
-                          child: Draggable(
-                            feedback: const CirclePoint(),
-                            onDragEnd: (dragDetails) {
-                              RenderBox box = stackKey.currentContext!
-                                  .findRenderObject() as RenderBox;
-                              Offset localOffset =
-                                  box.globalToLocal(dragDetails.offset);
-                              setState(() {
-                                point[0] = localOffset + const Offset(5, 5);
-                              });
-                            },
-                            child: const CirclePoint(),
+                          CustomPaint(
+                            painter: Lines(points),
                           ),
-                        ),
-                        Positioned(
-                          left: point[1].dx - 5,
-                          top: point[1].dy - 5,
-                          child: Draggable(
-                            feedback: const CirclePoint(),
-                            onDragEnd: (dragDetails) {
-                              RenderBox box = stackKey.currentContext!
-                                  .findRenderObject() as RenderBox;
-                              Offset localOffset =
-                                  box.globalToLocal(dragDetails.offset);
-                              setState(() {
-                                point[1] = localOffset + const Offset(5, 5);
-                              });
-                            },
-                            child: const CirclePoint(),
+                          Positioned(
+                            left: refPoint1.dx - 5,
+                            top: refPoint1.dy - 5,
+                            child: Draggable(
+                              feedback: const CirclePoint(),
+                              onDragEnd: (dragDetails) {
+                                RenderBox box = stackKey.currentContext!
+                                    .findRenderObject() as RenderBox;
+                                Offset localOffset =
+                                    box.globalToLocal(dragDetails.offset);
+                                setState(() {
+                                  refPoint1 =
+                                      localOffset + const Offset(5, 5) / zoom;
+                                });
+                              },
+                              child: const CirclePoint(),
+                            ),
                           ),
-                        ),
-                        Positioned(
-                          left: point[0].dx,
-                          top: point[0].dy,
-                          child: Transform.rotate(
-                            alignment: Alignment.topLeft,
-                            angle: atan2(point[1].dy - point[0].dy,
-                                point[1].dx - point[0].dx),
-                            child: Container(
-                              alignment: Alignment.center,
-                              width: distancePixel(point[0], point[1]),
-                              child: StrokeText(
-                                strokeWidth: 2,
-                                text: distance(point[0], point[1])
-                                    .toStringAsFixed(1),
+                          Positioned(
+                            left: refPoint2.dx - 5,
+                            top: refPoint2.dy - 5,
+                            child: Draggable(
+                              feedback: const CirclePoint(),
+                              onDragEnd: (dragDetails) {
+                                RenderBox box = stackKey.currentContext!
+                                    .findRenderObject() as RenderBox;
+                                Offset localOffset =
+                                    box.globalToLocal(dragDetails.offset);
+                                setState(() {
+                                  refPoint2 =
+                                      localOffset + const Offset(5, 5) / zoom;
+                                });
+                              },
+                              child: const CirclePoint(),
+                            ),
+                          ),
+                          Positioned(
+                            left: refPoint1.dx,
+                            top: refPoint1.dy,
+                            child: Transform.rotate(
+                              alignment: Alignment.topLeft,
+                              angle: atan2(refPoint2.dy - refPoint1.dy,
+                                  refPoint2.dx - refPoint1.dx),
+                              child: Container(
+                                alignment: Alignment.center,
+                                width: distancePixel(refPoint1, refPoint2),
+                                child: StrokeText(
+                                  strokeWidth: 2,
+                                  text: distance(refPoint1, refPoint2)
+                                      .toStringAsFixed(1),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ]
-                    ],
+                          for (List<Offset> point in points) ...[
+                            Positioned(
+                              left: point[0].dx - 5,
+                              top: point[0].dy - 5,
+                              child: Draggable(
+                                feedback: const CirclePoint(),
+                                onDragEnd: (dragDetails) {
+                                  RenderBox box = stackKey.currentContext!
+                                      .findRenderObject() as RenderBox;
+                                  Offset localOffset =
+                                      box.globalToLocal(dragDetails.offset);
+                                  setState(() {
+                                    point[0] =
+                                        localOffset + const Offset(5, 5) / zoom;
+                                  });
+                                },
+                                child: const CirclePoint(),
+                              ),
+                            ),
+                            Positioned(
+                              left: point[1].dx - 5,
+                              top: point[1].dy - 5,
+                              child: Draggable(
+                                feedback: const CirclePoint(),
+                                onDragEnd: (dragDetails) {
+                                  RenderBox box = stackKey.currentContext!
+                                      .findRenderObject() as RenderBox;
+                                  Offset localOffset =
+                                      box.globalToLocal(dragDetails.offset);
+                                  setState(() {
+                                    point[1] =
+                                        localOffset + const Offset(5, 5) / zoom;
+                                  });
+                                },
+                                child: const CirclePoint(),
+                              ),
+                            ),
+                            Positioned(
+                              left: point[0].dx,
+                              top: point[0].dy,
+                              child: Transform.rotate(
+                                alignment: Alignment.topLeft,
+                                angle: atan2(point[1].dy - point[0].dy,
+                                    point[1].dx - point[0].dx),
+                                child: Container(
+                                  alignment: Alignment.center,
+                                  width: distancePixel(point[0], point[1]),
+                                  child: StrokeText(
+                                    strokeWidth: 2,
+                                    text: distance(point[0], point[1])
+                                        .toStringAsFixed(1),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ]
+                        ],
+                      ),
+                    ),
                   ),
           )),
           const VerticalDivider(),
@@ -193,6 +215,25 @@ class _ImportImageState extends State<ImportImage> {
                 verticalSpace,
                 Text(
                     'Ref length: ${distancePixel(refPoint1, refPoint2).toStringAsFixed(1)} px'),
+                verticalSpace,
+                isSaving
+                    ? const CircularProgressIndicator()
+                    : ElevatedButton(
+                        onPressed: () async {
+                          setState(() {
+                            isSaving = true;
+                          });
+                          final bytes = await captureController.capture();
+
+                          await WebImageDownloader.downloadImageFromUInt8List(
+                              uInt8List: bytes!, name: 'image');
+
+                          setState(() {
+                            isSaving = false;
+                          });
+                        },
+                        child: const Text('Save'),
+                      ),
                 verticalSpace,
                 const Row(
                   children: [
